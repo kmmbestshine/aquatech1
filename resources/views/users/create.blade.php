8e755fba8e978581d5f4a3f023b2262b3f3fe87f
example1 <32572574+bestshine@users.noreply.github.com>
2020-12-22 20:17:46 +0530

    initial commit
    initial commit

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('heading')
+    {{ __('Create user') }}
+@stop
+
+@section('content')
+    {!! Form::open([
+            'route' => 'users.store',
+            'files'=>true,
+            'enctype' => 'multipart/form-data'
+
+            ]) !!}
+    @include('users.form', ['submitButtonText' => __('Create user')])
+
+    {!! Form::close() !!}
+
+
+@stop
